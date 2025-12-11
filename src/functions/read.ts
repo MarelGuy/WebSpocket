ff749c2c71e8ae575ce68be4c9c648b876ee0f99
MarelGuy <irertnuc.loris@gmail.com>
2025-12-11 09:15:41 +0100

    using buffer instead of fixed size array

--- src/functions/read.ts
+++ src/functions/read.ts
@@ -1,11 +1,21 @@
+import { Buffer } from "@std/io/buffer";
+
 const read = async (
   conn: Deno.Conn | Deno.TlsConn,
 ): Promise<{ bytesRead: number | null; buffer: Uint8Array }> => {
-  const buffer = new Uint8Array(65536);
+  const buf = new Buffer();
+  const tempChunk = new Uint8Array(4096);
 
-  const bytesRead = await conn.read(buffer);
+  const bytesRead = await conn.read(tempChunk);
 
-  return { bytesRead, buffer };
+  if (bytesRead !== null) {
+    await buf.write(tempChunk.subarray(0, bytesRead));
+  }
+
+  return {
+    bytesRead,
+    buffer: buf.bytes(),
+  };
 };
 
 export { read };
